ba2027fa851022fee91910052183ce41935eb310
Kennet Vuong <me@kennetvuong.com>
2015-12-02 19:51:10 +0100

    Done luke2

--- src/luke2/Luke2.kt
+++ src/luke2/Luke2.kt
@@ -7,7 +7,8 @@ import java.io.File
  */
 
 fun main(args: Array<String>) {
-    var list : List<String> = File("files/sJVZp7BH.txt").readLines();
-
-
+    var max = 0.0;
+    val l: List<Double> = File("files/sJVZp7BH.txt").readLines().map { it.toDouble() }
+    l.forEachIndexed { i, d -> l.subList(i, l.size).filter { it > d }.forEach {max = if (it - d > max) it - d else max}}
+    println(max);
 }
