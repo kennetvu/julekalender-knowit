1345ffb694838d0a1329599dfaf1f6268db378ed
kenvuo <kenvuo@knowit.no>
2015-12-02 18:06:44 +0100

    Startet paa luke2

---
+++ src/luke2/Luke2.kt
@@ -0,0 +1,13 @@
+package luke2
+
+import java.io.File
+
+/**
+ * Created by kennetvuong on 02/12/15.
+ */
+
+fun main(args: Array<String>) {
+    var list : List<String> = File("files/sJVZp7BH.txt").readLines();
+
+
+}
